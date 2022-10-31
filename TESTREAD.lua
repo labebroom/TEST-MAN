22f2960c40afce0b5857cbb2fe1340c36d35d3aa
labebroom <83304936+labebroom@users.noreply.github.com>
2022-10-31 01:05:47 -0400

    Update TESTREAD.lua

--- TESTREAD.lua
+++ TESTREAD.lua
@@ -481,8 +481,8 @@ function OrionLib:MakeWindow(WindowConfig)
 	WindowConfig.IntroText = WindowConfig.IntroText or "Kyverst"
 	WindowConfig.CloseCallback = WindowConfig.CloseCallback or function() end
 	WindowConfig.ShowIcon = WindowConfig.ShowIcon or false
-	WindowConfig.Icon = WindowConfig.Icon or "rbxassetid://8834748103"
-	WindowConfig.IntroIcon = WindowConfig.IntroIcon or "rbxassetid://8834748103"
+	WindowConfig.Icon = WindowConfig.Icon or "rbxassetid://11429668637"
+	WindowConfig.IntroIcon = WindowConfig.IntroIcon or "rbxassetid://11429668637"
 	OrionLib.Folder = WindowConfig.ConfigFolder
 	OrionLib.SaveCfg = WindowConfig.SaveConfig
 
